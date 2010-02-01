98ff7bf5b0b9d116552bb09f14c028e167d38cdf
Praveen Kumar <praveen@cpan.org>
2010-02-01 02:46:23 +0000

    Import of PRAVEEN/Text-CSV-Basic-1.01 from CPAN.
    gitpan-cpan-distribution: Text-CSV-Basic
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         PRAVEEN/Text-CSV-Basic-1.01.tar.gz
    gitpan-cpan-author:       PRAVEEN
    gitpan-cpan-maturity:     released

--- lib/Text/CSV/Basic.pm
+++ lib/Text/CSV/Basic.pm
@@ -2,7 +2,7 @@ package Text::CSV::Basic;
 use strict;
 
 BEGIN {
-    $Text::CSV::Basic::VERSION = '1.00';
+    $Text::CSV::Basic::VERSION = '1.01';
 }
 
 
@@ -99,7 +99,7 @@ Text::CSV::Basic - Get data from Text CSV file using header field.
 
 =head1 VERSION
 
-    1.00 
+    1.01 
 
 =head1 SYNOPSIS
 
